9be0c2c72637be856a0025911b9849f110ce841d
gap4545 <gap4545@gmail.com>
2022-04-04 16:08:39 -0400

    Welcome Screen Styling

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 let gameModuleContainer = document.querySelector('.module-container');
 
-function animateOnce(animationClass, element, deleteElement) {
+function animateOnce(animationClass, element, deleteElement = false, removeClass = false) {
+
     element.classList.add(animationClass);
     element.addEventListener('animationend', e => {
-        element.classList.remove(animationClass);
         if (deleteElement == true) {
             element.parentNode.removeChild(element);
         };
+        if (removeClass == true) {
+            element.classList.remove(animationClass);
+        };
     }, {once : true});
+
 };
 
 const game = (function() {
@@ -28,6 +32,7 @@ const game = (function() {
         })
     
         return {getVal, type};
+
     });
 
     const _Computer = (function(val) {
@@ -41,6 +46,7 @@ const game = (function() {
         //Define computer logic here
 
         return {getVal, type};
+
     });
     
     const _getTurn = (function() {
@@ -48,8 +54,10 @@ const game = (function() {
     });
     
     const _changeTurn = (function() {
+
         turn = (turn === 0) ? 1 : 0; // If turn equals 0 turn becomes 1 if not turn equals 0;
         console.log(turn);
+
     });
     
     const _playAgainScreen = (function() {
@@ -60,13 +68,16 @@ const game = (function() {
         let playNewButton = document.createElement('button');
 
         const _destroy = (function () {
-            animateOnce('shrink', winnerDiv, true);
-            animateOnce('shrink', playSameButton, true);
-            animateOnce('shrink', playNewButton, true);
-            animateOnce('fade-out', playAgainDiv, true);
+
+            animateOnce('shrink', winnerDiv, true, true);
+            animateOnce('shrink', playSameButton, true, true);
+            animateOnce('shrink', playNewButton, true, true);
+            animateOnce('fade-out', playAgainDiv, true, true);
+
         });
 
         const _creation = (function() {
+
             playAgainDiv.classList.add('play-again-container');
     
             winnerDiv.classList.add('winner-text');
@@ -88,19 +99,22 @@ const game = (function() {
             });
     
             animateOnce('fade-in', playAgainDiv, false);
-            animateOnce('expand-bounce', winnerDiv, false);
+            animateOnce('bounce', winnerDiv, false, true);
             animateOnce('expand', playSameButton, false);
             animateOnce('expand', playNewButton, false);
     
         })();
 
         const display = (function (winningPlayerVal) {
+
             winnerDiv.textContent = `Player ${Number(winningPlayerVal) + 1} Wins!`;
             gameModuleContainer.append(playAgainDiv);
             playAgainDiv.append(winnerDiv, playSameButton, playNewButton);
+
         });
 
         return {display};
+
     })();
 
     const gameboard = (function() {
@@ -109,14 +123,18 @@ const game = (function() {
         const boardArr = [];
 
         const setSpace = (function(spaceButton) {
+
             spaceButton.value = players[_getTurn()].getVal();
             spaceButton.classList.remove('open');
             spaceButton.classList.add('taken');
             spaceButton.textContent = spaceButton.value;
-        })
+
+        });
 
         const reset = (function() {
+
             let i = 5;
+
             boardArr.forEach(spaceButton => {
                 spaceButton.disabled = false;
                 spaceButton.textContent = '';
@@ -124,10 +142,13 @@ const game = (function() {
                 spaceButton.className = 'open space';
                 i++;
             });
+
         });
     
         const _init = (function(){
+
             const spaceButton = document.createElement('button');
+
             for (let i = 0; i <= 9; i++) boardArr.push(spaceButton.cloneNode());
             boardArr.forEach(spaceButton => {
                 spaceButton.addEventListener('click', eve => {
@@ -136,25 +157,32 @@ const game = (function() {
                     checkWin();
                     spaceButton.disabled = true;
                 });
-            })
+            });
+
             reset();
+
         })();
     
         const displayBoard = (function() {
+
             gameContainer.className = 'game-container';
             boardArr.forEach(e => {
                 gameContainer.appendChild(e);
             });
             gameModuleContainer.append(gameContainer);
+
         });
 
         const destroy = (function() {
+
             reset();
             gameModuleContainer.removeChild(gameContainer);
+
         });
     
     
         const checkWin = (function() {
+
             if (
                 (boardArr[4].value === boardArr[0].value && boardArr[4].value === boardArr[8].value) |
                 (boardArr[4].value === boardArr[1].value && boardArr[4].value === boardArr[7].value) |
@@ -173,28 +201,30 @@ const game = (function() {
         });
         
         return {displayBoard, reset, destroy};
+
     })();
     
     const start = (function() {
+
         turn = Math.floor(Math.random() * 2); // Set turn to a random number from 0 to (players.length-1)
         gameModuleContainer.classList.add('game-screen');
         gameboard.displayBoard();
+
     });
 
     //Makes new player with the value of the length of the players array to keep it simple in getting each player for expansion purposes
     const createPlayer = (function(playerType) {
+
         if (playerType === 'player') {
             players.push(_Player(players.length));
         } else if (playerType === 'computer') {
             players.push(_Computer(players.length))
         };
+
     });
 
-    const logPlayers = (function() {
-        console.log(players);
-    })
+    return {createPlayer, start, _getTurn};
 
-    return {createPlayer, start, logPlayers, _getTurn};
 })();
 
 const welcomeScreen = (function() {
@@ -206,34 +236,45 @@ const welcomeScreen = (function() {
     let playerSelectorDiv = document.createElement('div');
 
     const _init = (function() {
+
         gameModuleContainer.classList.add('welcome-screen');
         header.classList.add('header');
         title.classList.add('game-title');
         welcomeTo.classList.add('welcome-to-span');
         playerSelectorDiv.classList.add('player-forms-container');
         underlineAnimation.classList.add('underline-animate');
+
         title.textContent = 'TIC-TAC-TOE';
         welcomeTo.textContent = 'Welcome to ';
         welcomeTo.addEventListener('animationend', function() {
             underlineAnimation.classList.add('expand')
         }, {once : true});
-        animateOnce('bounce-from-top', welcomeTo, false);
-        header.append(welcomeTo, title);
+        animateOnce('bounce-from-top', welcomeTo, false, true);
+
+        title.append(welcomeTo)
+        header.append(title);
+
     })();
 
     const _destroy = (function() {
+
         animateOnce('slide-left', welcomeTo, true);
         animateOnce('slide-down', playerSelectorDiv, true);
         gameModuleContainer.classList.remove('welcome-screen');
+
     });
 
     const _display = (function() {
+
         welcomeTo.appendChild(underlineAnimation);
         gameModuleContainer.append(header, playerSelectorDiv);
+
     });
 
     const _getPlayers = (function() {
+
         let forms = gameModuleContainer.querySelectorAll('.player-form');
+
         for (let i = 1; i <= forms.length; i++) {
             if (gameModuleContainer.querySelector(`#player${i}`).checked === true) {
                 game.createPlayer('player');
@@ -241,42 +282,45 @@ const welcomeScreen = (function() {
                 game.createPlayer('computer');
             };
         };
+
     });
 
     const _start = (function() {
+
         _getPlayers();
         _destroy();
         game.start();
+
     });
 
     const _createForm = (function() {
+
         for (let i = 1; i <= 2; i++) {
             let playerForm = document.createElement('form');
-            playerForm.className = 'player-form';
+            playerForm.classList.add('player-form');
             playerForm.setAttribute('name', `player${i}-form`);
-    
-            let playerRadioLabel = document.createElement('label');
-            playerRadioLabel.setAttribute('for', `player${i}`);
-            playerRadioLabel.textContent = 'Player';
+
+            let formHeader = document.createElement('h3');
+            formHeader.classList.add('player-form-header');
+            formHeader.textContent = `Player ${i}`;
+
     
             let playerRadioButton = document.createElement('input');
+            playerRadioButton.classList.add('player-radio-button');
             playerRadioButton.setAttribute('type', 'radio');
             playerRadioButton.setAttribute('name', `player${i}-radio`)
             playerRadioButton.setAttribute('id', `player${i}`)
             playerRadioButton.setAttribute('value', 'player');
             playerRadioButton.setAttribute('checked', 'true');
     
-            let computerRadioLabel = document.createElement('label');
-            computerRadioLabel.setAttribute('for', `computer${i}`);
-            computerRadioLabel.textContent = 'Computer';
-    
             let computerRadioButton = document.createElement('input');
+            computerRadioButton.classList.add('computer-radio-button');
             computerRadioButton.setAttribute('type', 'radio');
             computerRadioButton.setAttribute('name', `player${i}-radio`);
             computerRadioButton.setAttribute('id', `computer${i}`);
             computerRadioButton.setAttribute('value', 'computer');
     
-            playerForm.append(playerRadioLabel, playerRadioButton, computerRadioLabel, computerRadioButton);
+            playerForm.append(formHeader, playerRadioButton, computerRadioButton);
             playerSelectorDiv.appendChild(playerForm);
         };
 
@@ -286,13 +330,18 @@ const welcomeScreen = (function() {
         startGameButton.addEventListener('click', _start);
         playerSelectorDiv.append(startGameButton);
 
+        animateOnce('expand', playerSelectorDiv, false, false);
         _display();
+
     })();
 
     const reset = (function() {
+
         header.insertBefore(welcomeTo, title);
         _display();
+
     });
 
     return {reset};
+
 })();
